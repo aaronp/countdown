7264ab8fa1cb6ecf90c3fb567783506afc29e8df
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2019-10-18 11:24:21 +0100

    seedTestFix

--- src/test/scala/ga/SeedTest.scala
+++ src/test/scala/ga/SeedTest.scala
@@ -4,30 +4,37 @@ class SeedTest extends BaseSpec {
 
   "Seed.nextDouble" should {
 
-    val seedAndDouble = Seq(
-      (6997317046046405250L, 0.500009849503075),
-      (3092481303161922626L, 9.974837703378644E-5),
-      (6129515119770464689L, 0.9999998781248554)
-    )
-    seedAndDouble.foreach {
+    Seq(
+      (Seed(-4611776864058626593L), 0.500009849503075),
+      (Seed(-920016391455073L), 9.974837703378644E-5),
+      (Seed(-9223370912754974934L), 0.9999998781248554)
+    ).foreach {
       case (seed, expected) =>
+        s"return $expected for seed ${seed.long}" in {
+          Seed.nextDouble.run(seed).value._2 shouldBe expected
+        }
+    }
 
-        s"return $expected for seed $seed" in {
-          val (_, found) = Iterator.from(0).take(10000000).foldLeft((Seed(0), Seq.empty[(Seed, Double)])) {
+    val ranges = Seq(
+      (0.0, 0.0001),
+      (0.4999, 0.50001),
+      (0.999, 1.0)
+    )
+    ranges.foreach {
+      case (from, to) =>
+
+        s"return values between $from and $to for some value" in {
+          val (_, found) = Iterator.from(0).take(10000).foldLeft((Seed(0), Seq.empty[(Seed, Double)])) {
             case ((s, found), _) =>
               val (next, value) = Seed.nextDouble.run(s).value
               val list = if (value >= from && value <= to) {
-                (next, value) +: found
+                (s, value) +: found
               } else {
                 found
               }
               (next, list)
           }
-          val midpoint = from + (to - from / 2)
-          val closest = found.minBy{
-            case (_, value) => (value - midpoint).abs
-          }
-          println(closest)
+          found should not be empty
         }
     }
     "return values between 0.0 and 1.0" in {
@@ -46,7 +53,7 @@ class SeedTest extends BaseSpec {
         val randoms = (0 to max * 2).map { i =>
           Seed.nextInt(max).run(Seed(i * 53).next).value._2
         }
-        randoms.distinct.sorted should contain theSameElementsInOrderAs (0 to max)
+        randoms.find(x => x > max || x < 0) shouldBe None
       }
     }
   }
