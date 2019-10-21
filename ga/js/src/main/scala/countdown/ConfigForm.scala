a686b521424f588f110ab527708a2db33b58eae5
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2019-10-21 21:27:30 +0100

    extracted ListElements

--- ga/js/src/main/scala/countdown/ConfigForm.scala
+++ ga/js/src/main/scala/countdown/ConfigForm.scala
@@ -2,10 +2,10 @@ package countdown
 
 import ga.GeneticAlgo.Generation
 import ga.Seed
+import org.scalajs.dom.html.Form
 import org.scalajs.dom.window
+import scalatags.JsDom
 import scalatags.JsDom.all.{`class`, _}
-
-import scala.util.Try
 
 /**
   * Rendering of our configuration form
@@ -13,134 +13,57 @@ import scala.util.Try
 object ConfigForm {
 
   def apply(logGeneration: Generation[Equation] => Unit,
-            onSolve: (CountdownConfig, Int) => Unit) = {
-
-    def makeLi(field: String, initialValue: String, hint: String) = {
-      val fieldId = field.filter(_.isLetterOrDigit).toLowerCase
-
-      val inp = input(`type` := "text",
-                      name := fieldId,
-                      maxlength := "20",
-                      value := initialValue).render
-      val liElm = li(
-        label(`for` := fieldId)(field),
-        inp,
-        span(hint)
-      )
-      (liElm, inp)
-    }
-
-    val (tgtNrLi, tgtNr) =
-      makeLi("Target Number", "12", "The number we're trying to find")
-    val (usingLi, using) =
-      makeLi("Using Input Numbers",
-             "3 4 6 1",
-             "The numbers available to reach the target numbrer")
-    val (seedLi, seed) = makeLi(
-      "Random Value Seed",
-      "",
-      "An optional value with which to initialise our random value generator")
-    val (maxGenLi, maxGen) = makeLi(
-      "Max Generations",
-      "200",
-      "How many generations to allow before we quit without an answer")
-    val (popSizeLi, popSize) = makeLi(
-      "Population Size",
-      "100",
-      "How large we should allow the population to grow")
-
-    val (mutationProbLi, mutationProb) = makeLi(
-      "Mutation Probability",
-      "0.01",
-      "A number between 0.0 and 1.0 representing the probability of a mutation")
-
-    val (minEqSizeLi, minEqSize) = makeLi(
-      "Minimum Equation Size",
-      "1",
-      "The smallest equation length to use in the initial population")
-
-    val (maxNodesLi, maxNodes) =
-      makeLi("Node Limit", "20", "The maximum number of nodes to render")
-
-    def inputNumbers = {
-      Try(using.value.split(",").flatMap(_.split(" ")).map(_.toInt).toSet)
-    }
-
-    using.onkeyup = _ => {
-      inputNumbers.foreach { nrs =>
-        window.console.log(
-          s"inputNumbers=$inputNumbers, minEqSize.value='${minEqSize.value}'")
-
-        Try(minEqSize.value.toInt).foreach { minSize =>
-          if (minSize >= nrs.size) {
-            minEqSize.value = nrs.size.toString
-          }
-        }
-      }
-    }
+            onSolve: (CountdownConfig, Int) => Unit): JsDom.TypedTag[Form] = {
+    import FormElement.configListItems
 
     val submit = input(`type` := "submit", value := "Solve").render
+
     submit.onclick = e => {
       window.console.info("solve click")
       e.preventDefault()
       e.stopPropagation()
 
-      val targetNumber = tgtNr.value.toInt
+      for {
+        (seed, settings) <- configListItems.asSettings
+        inputNrs <- configListItems.inputNumbers.currentValue()
+        targetValue <- configListItems.targetNr.currentValue()
+        minEqSize <- configListItems.minEquationSize.currentValue()
+        maxNodes <- configListItems.maxNodes.currentValue()
+      } {
+        val rnd = seed.getOrElse(System.currentTimeMillis())
+        // stamp our random seed
+        FormElement.configListItems.seed.inputElement.value = rnd.toString
+        window.console.info(s"settings is $settings")
 
-      val settings = CountdownConfig.makeAlgoSettings(
-        targetNumber = targetNumber,
-        inputNumbers = inputNumbers.getOrElse(Set.empty),
-        maxPopulation = popSize.value.toInt,
-        mutationProbability = mutationProb.value.toDouble,
-        maxGenerations = maxGen.value.toInt
-      )
+        val countdownCfg = new CountdownConfig(
+          settings = settings,
+          rand = Seed(rnd),
+          inputValues = inputNrs,
+          targetValue = targetValue,
+          logGeneration,
+          minEquationSize = minEqSize,
+          maxEquationSize = inputNrs.size,
+          None
+        )
 
-      val rnd = {
-        val lngValue =
-          Try(seed.value.toLong).getOrElse(System.currentTimeMillis)
-        Seed(lngValue)
+        window.console.info(s"CountdownConfig is $countdownCfg")
+
+        onSolve(countdownCfg, maxNodes)
       }
-      window.console.info(s"""tgtNr.value='${tgtNr.value}'
-           |using.value='${using.value}', inputNumbers -> $inputNumbers
-           |maxPopulation='${popSize.value}'
-           |mutationProbability='${mutationProb.value}'
-           |minEqSize='${minEqSize.value}'
-           |maxGenerations='${maxGen.value}'
-           |seed='${seed.value}'
-           |""".stripMargin)
-
-      window.console.info(s"settings is $settings")
-
-      val countdownCfg = new CountdownConfig(
-        settings = settings,
-        rand = rnd,
-        inputValues = inputNumbers.getOrElse(Set.empty),
-        targetValue = targetNumber,
-        logGeneration,
-        minEquationSize = minEqSize.value.toInt,
-        maxEquationSize = inputNumbers.getOrElse(Set.empty).size,
-        None
-      )
-
-      window.console.info(s"CountdownConfig is $countdownCfg")
-
-      // ensure we stamp our 'seed' value for future computations
-      seed.value = rnd.long.toString
-
-      onSolve(countdownCfg, maxNodes.value.toInt)
     }
 
     form(`class` := "form-style-7")(
       ul(
-        tgtNrLi,
-        usingLi,
-        seedLi,
-        maxGenLi,
-        popSizeLi,
-        mutationProbLi,
-        minEqSizeLi,
-        maxNodesLi,
+        configListItems.targetNr.liElement,
+        configListItems.inputNumbers.liElement,
+        configListItems.seed.liElement,
+        configListItems.maxGen.liElement,
+        configListItems.populationSize.liElement,
+        configListItems.mutationProbability.liElement,
+        configListItems.minEquationSize.liElement,
+        configListItems.maxNodes.liElement,
         li(submit)
       ))
   }
+
 }
--- ga/js/src/main/scala/countdown/FormElement.scala
+++ ga/js/src/main/scala/countdown/FormElement.scala
@@ -1,3 +1,158 @@
 package countdown
 
-final case class FormElement() {}
+import ga.AlgoSettings
+import scalatags.JsDom.all.{value, _}
+
+import scala.util.{Failure, Success, Try}
+
+final case class FormElement[A](field: String,
+                                initialValue: String,
+                                hint: String,
+                                validate: String => Either[String, A]) {
+  val fieldId = field.filter(_.isLetterOrDigit).toLowerCase
+
+  val inputElement = input(`type` := "text",
+                           name := fieldId,
+                           maxlength := "20",
+                           value := initialValue).render
+
+  inputElement.onkeyup = _ => {
+    validate(textValue) match {
+      case Left(msg) => validationSpan.innerHTML = msg
+      case Right(_)  => validationSpan.innerHTML = ""
+    }
+  }
+  private val validationSpan = span("validation goes here").render
+  val liElement = li(
+    label(`for` := fieldId)(field),
+    inputElement,
+    span(hint),
+    validationSpan,
+  )
+
+  def currentValue(): Option[A] = validate(textValue).toOption
+
+  private def textValue = inputElement.value
+}
+
+object FormElement {
+
+  private def valueAsNumbers(textValue: String): Try[Set[Int]] = {
+    Try(textValue.split(",").flatMap(_.split(" ")).map(_.toInt).toSet)
+  }
+
+  def int(field: String,
+          initialValue: String,
+          hint: String,
+          min: Int = 0,
+          max: Int = 500) = {
+    def asInt(value: String) = Try(value.trim.toInt) match {
+      case Failure(_)                         => Left("invalid integer")
+      case Success(a) if a >= min && a <= max => Right(a)
+      case Success(a)                         => Left(s"$a should be between ${min} and $max")
+    }
+
+    new FormElement(field, initialValue, hint, asInt)
+  }
+
+  def double(field: String,
+             initialValue: String,
+             hint: String,
+             min: Double,
+             max: Double) = {
+    def asInt(value: String) = Try(value.trim.toDouble) match {
+      case Failure(_)                         => Left("invalid double")
+      case Success(a) if a >= min && a <= max => Right(a)
+      case Success(a)                         => Left(s"$a should be between ${min} and $max")
+    }
+
+    new FormElement(field, initialValue, hint, asInt)
+  }
+
+  def optLong(field: String,
+              initialValue: String,
+              hint: String): FormElement[Option[Long]] = {
+    def asInt(value: String) = Try(value.trim.toLong) match {
+      case Failure(_) if value.trim.isEmpty => Right(Option.empty[Long])
+      case Failure(_)                       => Left("invalid integer")
+      case Success(a)                       => Right(Option(a))
+    }
+
+    new FormElement(field, initialValue, hint, asInt)
+  }
+
+  def ints(field: String, initialValue: String, hint: String) = {
+    def asInt(value: String) = valueAsNumbers(value.trim) match {
+      case Failure(_) =>
+        Left("required a space or comma-separated list of integers")
+      case Success(a) => Right(a)
+    }
+
+    new FormElement[Set[Int]](field, initialValue, hint, asInt)
+  }
+
+  object configListItems {
+
+    val targetNr =
+      FormElement.int("Target Number", "12", "The number we're trying to find")
+    val inputNumbers =
+      FormElement.ints("Using Input Numbers",
+                       "3 4 6 1",
+                       "The numbers available to reach the target number")
+    val seed = FormElement.optLong(
+      "Random Value Seed",
+      "",
+      "An optional value with which to initialise our random value generator")
+    val maxGen = FormElement.int(
+      "Max Generations",
+      "200",
+      "How many generations to allow before we quit without an answer")
+    val populationSize = FormElement.int(
+      "Population Size",
+      "100",
+      "How large we should allow the population to grow")
+
+    val mutationProbability = FormElement.double(
+      "Mutation Probability",
+      "0.01",
+      "A number between 0.0 and 1.0 representing the probability of a mutation",
+      0.0,
+      1.0)
+
+    val minEquationSize = FormElement.int(
+      "Minimum Equation Size",
+      "1",
+      "The smallest equation length to use in the initial population")
+
+    val maxNodes =
+      FormElement.int("Solution Node Limit",
+                      "20",
+                      "The maximum number of nodes to render",
+                      1,
+                      50)
+
+    def asSettings: Option[(Option[Long], AlgoSettings[Equation])] = {
+      for {
+        targetNumber <- targetNr.currentValue()
+        inputNumbers <- inputNumbers.currentValue()
+        maxPopulation <- populationSize.currentValue()
+        mutationProbability <- mutationProbability.currentValue()
+        maxGenerations <- maxGen.currentValue()
+        seed <- seed.currentValue()
+      } yield {
+
+        val settings = CountdownConfig.makeAlgoSettings(
+          targetNumber = targetNumber,
+          inputNumbers = inputNumbers,
+          maxPopulation = maxPopulation,
+          mutationProbability = mutationProbability,
+          maxGenerations = maxGenerations
+        )
+        (seed, settings)
+      }
+
+      //.getOrElse(System.currentTimeMillis)
+    }
+  }
+
+}
