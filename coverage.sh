9e83844f7634fbb0f08f2a4fb3ae68f81546acbc
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2019-10-21 16:33:58 +0100

    cleanout

--- coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-sbt clean coverage test coverageReport
-
-cp -r target/scala-2.12/scoverage-report target
-
-echo "Seed ./target/scoverage-report/index.html"
-open ./target/scoverage-report/index.html
--- ga/shared/src/test/scala/ga/BaseSpec.scala
+++
@@ -1,5 +0,0 @@
-package ga
-
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
-
-abstract class BaseSpec extends WordSpec with Matchers with BeforeAndAfterAll
