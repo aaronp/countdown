a7ae6dfb3f04fbe6d996f48023e9135e8b2fc37f
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2019-10-22 14:57:11 +0100

    MainTest fix

--- ga/jvm/src/test/scala/countdown/MainTest.scala
+++ ga/jvm/src/test/scala/countdown/MainTest.scala
@@ -8,12 +8,12 @@ class MainTest extends BaseSpec {
     "write an html solution when output.dir is set" in {
       val dir = s"target/temp-${System.currentTimeMillis}".asPath.mkDirs()
       try {
-        val solnOpt = Main.runMain(Array(s"output.dir=${dir.toAbsolutePath}")).flatten
-        solnOpt.isDefined shouldBe true
+        val Some(soln) = Main.runMain(Array(s"output.dir=${dir.toAbsolutePath}", "target=12", "from=3,4,5", "seed=1234")).flatten
+        dir.children.map(_.fileName) should contain ("solution.html")
+        soln.value.expressionString shouldBe "3 + 5 + 4"
       } finally {
         dir.delete()
       }
     }
   }
-
 }
