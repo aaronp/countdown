b48631cb2bbe7b852afafb456813ba41d524350a
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-09-30 16:06:55 +0100

    reset version

--- ga/jvm/src/main/scala/countdown/rest/CountdownRoutes.scala
+++ ga/jvm/src/main/scala/countdown/rest/CountdownRoutes.scala
@@ -15,11 +15,11 @@ import io.circe.syntax._
 
 object CountdownRoutes {
 
-  implicit def requestEntityDecoder[F[_] : Sync]
-  : EntityDecoder[F, CountdownRequest] = jsonOf
+  implicit def requestEntityDecoder[F[_]: Sync]
+    : EntityDecoder[F, CountdownRequest] = jsonOf
 
-  implicit def responseEntityEncoder[F[_] : Applicative]
-  : EntityEncoder[F, CountdownResponse] = jsonEncoderOf
+  implicit def responseEntityEncoder[F[_]: Applicative]
+    : EntityEncoder[F, CountdownResponse] = jsonEncoderOf
 
   object SeedParam extends OptionalQueryParamDecoderMatcher[Long]("seed")
 
@@ -27,8 +27,8 @@ object CountdownRoutes {
 
   object NumbersParam extends QueryParamDecoderMatcher[String]("numbers")
 
-  def handle[F[_] : Sync](svc: Service[F],
-                          request: CountdownRequest): F[Response[F]] = {
+  def handle[F[_]: Sync](svc: Service[F],
+                         request: CountdownRequest): F[Response[F]] = {
     val dsl = Http4sDsl[F]
     import dsl._
     for {
@@ -37,23 +37,22 @@ object CountdownRoutes {
     } yield resp
   }
 
-  def apply[F[_] : Sync](svc: Service[F]): HttpRoutes[F] = {
+  def apply[F[_]: Sync](svc: Service[F]): HttpRoutes[F] = {
     val dsl = Http4sDsl[F]
     import dsl._
     HttpRoutes.of[F] {
       case GET -> Root / IntVar(target) :? NumbersParam(numbers) +& DebugParam(
-      debug) +& SeedParam(seedOpt) =>
+            debug) +& SeedParam(seedOpt) =>
         val request =
           CountdownRequest(target,
-            numbers.split(",", -1).map(_.trim.toInt).toSet,
-            seedOpt,
-            includeWorkingsOut = debug.getOrElse(false))
+                           numbers.split(",", -1).map(_.trim.toInt).toSet,
+                           seedOpt,
+                           includeWorkingsOut = debug.getOrElse(false))
         handle(svc, request)
-      case req@POST -> Root =>
+      case req @ POST -> Root =>
         req.as[CountdownRequest].flatMap(handle(svc, _))
       case GET -> Root =>
-        Ok(
-          s"""Usage:
+        Ok(s"""Usage:
              |GET /<target number>?numbers=<comma-separated numbers>&seed=1234
              |POST /
              |${CountdownRequest.example.asJson.spaces4}
